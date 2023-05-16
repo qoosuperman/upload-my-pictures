1196b1a8be0b91be9cde0da1c0364c7290d3ce55
anthonychao <qoosuperman@gmail.com>
2023-05-16 22:57:21 +0800

    config: configure uploads routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # 目前這個 app 只打算讓少部分人登入使用
   devise_for :users, only: :sessions
-  root to: "home#index"
+  root to: 'uploads#new'
+  resources :uploads, only: [:new, :create, :index]
 end
